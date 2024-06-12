d973cb18061ba7493641f73269ef825fb7937ec9
magdala-web <media2@magdala.org>
2024-06-12 14:39:01 +0300

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -1,1 +1,14 @@
+$(document).ready(function(){
 
+  var cookieName = "magdala-cookie-accept";
+  var cookieCloseBtn = $("button[u-cookie='close-button']");
+  var cookieAcceptBtn = $("button[u-cookie='accept-button']");
+  var cookieWrap = $("div[u-cookie='cookie-wrapper']");
+
+  if (Cookies.get(cookieName) === undefined) cookieWrap.show();
+
+  $(cookieAcceptBtn.add(cookieCloseBtn)).on('click', function(){
+    Cookies.set(cookieName, 'accept', { expires: 365 });
+    cookieWrap.hide();
+  });
+});
