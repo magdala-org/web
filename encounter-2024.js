b5ab419bb6bad3b6acd3c7877c7f661cd926e266
magdala-org <media2@magdala.org>
2024-08-20 11:22:51 -0600

    Fix encounter-2024.js

--- encounter-2024.js
+++ encounter-2024.js
@@ -1,37 +1,37 @@
 let classNavWrapper = $(".class_episode_nav_wrapper");
-let classNavBtn = $("button[u-class-element='nav-btn']");
+let classNavBtn = $("button[u-tabs-element='nav-btn']");
 let classNavBtnActive;
-let classContent = $("div[u-class-element='content']");
+let classContent = $("div[u-tabs-element='content']");
 let youtube_id;
 let classIndex = 0;
 
 
 classNavBtnActive = classNavBtn.eq(classIndex);
-youtube_id = classNavBtnActive.attr('u-class-youtubeid');
+youtube_id = classNavBtnActive.attr('u-tabs-youtubeid');
 
 classContent.hide();
 classContent.eq(classIndex).show();
 
 classNavBtnActive.addClass("class_episode_nav_btn_active");
 
-$("#" + youtube_id + "> iframe").attr('src', "https://www.youtube-nocookie.com/embed/"+youtube_id);
+$("div[u-tabs-element='content'], div[u-tabs-youtubeid='" + youtube_id + "'] > iframe").attr("src", "https://www.youtube-nocookie.com/embed/"+youtube_id);
 
 
 classNavBtn.on("click", function () {
   
-  $('.class_episode_video > iframe').each(function() {
+  $("div[u-tabs-element='content'] > iframe").each(function() {
     $(this).attr('src', '');
   });
   
-	classNavBtnActive.removeClass("class_episode_nav_btn_active");
-	classNavBtnActive = $(this);
+  classNavBtnActive.removeClass("class_episode_nav_btn_active");
+  classNavBtnActive = $(this);
   classIndex = classNavBtnActive.parent().index();
   
-  youtube_id = classNavBtnActive.attr('u-class-youtubeid');
+  youtube_id = classNavBtnActive.attr("u-tabs-youtubeid");
   classContent.hide();
-	classContent.eq(classIndex).show();
+  classContent.eq(classIndex).show();
   classNavBtnActive.addClass("class_episode_nav_btn_active");
   classNavWrapper.scrollTo(classNavBtnActive, 300);
   $("body").scrollTo($("#class-episode"), 300, {offset:-52});
-  $(".class_episode_video, div[u-class-youtubeid='" + youtube_id + "'] > iframe").attr('src', "https://www.youtube-nocookie.com/embed/"+youtube_id);  
+  $("div[u-tabs-element='content'], div[u-tabs-youtubeid='" + youtube_id + "'] > iframe").attr("src", "https://www.youtube-nocookie.com/embed/"+youtube_id);  
 });
