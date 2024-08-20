27aa394281977a9de9400eea986fc3297a959ee4
magdala-org <media2@magdala.org>
2024-08-20 11:48:09 -0600

    Fix encounter-2024.js

--- encounter-2024.js
+++ encounter-2024.js
@@ -1,4 +1,3 @@
-let classNavWrapper = $(".class_episode_nav_wrapper");
 let classNavBtn = $("button[u-tabs-element='nav-btn']");
 let classNavBtnActive;
 let classContent = $("div[u-tabs-element='content']");
@@ -8,14 +7,10 @@ let classIndex = 0;
 
 classNavBtnActive = classNavBtn.eq(classIndex);
 youtube_id = classNavBtnActive.attr('u-tabs-youtubeid');
-
 classContent.hide();
 classContent.eq(classIndex).show();
-
 classNavBtnActive.addClass("class_episode_nav_btn_active");
-
-$("div[u-tabs-element='content'], div[u-tabs-youtubeid='" + youtube_id + "'] > iframe").attr("src", "https://www.youtube-nocookie.com/embed/"+youtube_id);
-
+classContent.children("iframe").attr("src", "https://www.youtube-nocookie.com/embed/"+youtube_id);
 
 classNavBtn.on("click", function () {
   
@@ -26,12 +21,10 @@ classNavBtn.on("click", function () {
   classNavBtnActive.removeClass("class_episode_nav_btn_active");
   classNavBtnActive = $(this);
   classIndex = classNavBtnActive.parent().index();
-  
   youtube_id = classNavBtnActive.attr("u-tabs-youtubeid");
   classContent.hide();
   classContent.eq(classIndex).show();
   classNavBtnActive.addClass("class_episode_nav_btn_active");
-  classNavWrapper.scrollTo(classNavBtnActive, 300);
-  $("body").scrollTo($("#class-episode"), 300, {offset:-52});
-  $("div[u-tabs-element='content'], div[u-tabs-youtubeid='" + youtube_id + "'] > iframe").attr("src", "https://www.youtube-nocookie.com/embed/"+youtube_id);  
+  $("body").scrollTo($("#encounter2024_videos"), 300, {offset:-52});
+  classContent.children("iframe").attr("src", "https://www.youtube-nocookie.com/embed/"+youtube_id);  
 });
